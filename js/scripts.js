542aa3a1af266360c91ab7c4e035bddcf2bfc405
Chris Swanner and Joel Adams <chris@>
2018-01-11 14:33:45 -0800

    add additional styling.

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,7 @@ $('#triangle').submit(function(event) {
   var sideInput1 = parseInt($("#input1").val());
   var sideInput2 = parseInt($("#input2").val());
   var sideInput3 = parseInt($("#input3").val());
-
-    debugger;
+  
   if ((sideInput1 === 0) || (sideInput2 === 0) || (sideInput3 === 0)) {
     $("#result").text("This is not a triangle");
   } else if ((sideInput1 === sideInput2) && (sideInput2 === sideInput3)) {
